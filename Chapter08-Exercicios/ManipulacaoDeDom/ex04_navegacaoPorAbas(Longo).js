592d828a2c074740a90c08112ef3643f328871c9
Marcio Junior <marciojsjunior.mj@gmail.com>
2025-09-11 09:12:43 -0300

    Add ex04 longo

--- Chapter08-Exercicios/ManipulacaoDeDom/ex04_navegacaoPorAbas(Longo).js
+++ Chapter08-Exercicios/ManipulacaoDeDom/ex04_navegacaoPorAbas(Longo).js
@@ -1,27 +1,22 @@
 const botoesAbas = document.querySelectorAll('.aba-link');
-const conteudosAbas = document.querySelectorAll('.aba-conteudo');
+const conteudosAbas = document.querySelectorAll('.aba-conteudo'); // Em ambos os casos, a máquina irá interpretar da seguinte forma: "Selecione todos os elementos que possuem a classe 'aba-link' ou 'aba-conteudo'". Portanto ela irá retornar uma lista (NodeList) com todos os elementos que possuem essas classes no HTML.
 
-function aoClicarNaAba(event) {
-    const botaoClicado = event.currentTarget;
-    const idConteudoClicado = botaoClicado.dataset.aba;
-
-    for (let i = 0; i < conteudosAbas.length; i++) {
-        conteudosAbas[i].classList.remove('active');
+function criarFuncaoDeClique(indice) {
+  return function() { // Função que será executada quando o botão for clicado, ela congela o valor de 'indice'que foi passado como o argumento.
+    for (let j = 0; j < conteudosAbas.length; j++) {
+      conteudosAbas[j].classList.remove('active');
     }
-    for (let i = 0; i < botoesAbas.length; i++) {
-        botoesAbas[i].classList.remove('active');
+    for (let j = 0; j < botoesAbas.length; j++) {
+      botoesAbas[j].classList.remove('active');
     }
 
-    const conteudoAlvo = document.getElementById(idConteudoClicado);
-
-    ativarAba(botaoClicado, conteudoAlvo);
+    botoesAbas[indice].classList.add('active');
+    conteudosAbas[indice].classList.add('active');
+  };
 }
 
-function ativarAba(botao, conteudo) {
-    conteudo.classList.add('active');
-    botao.classList.add('active');
+for (let i = 0; i < botoesAbas.length; i++) {
+  const botaoAtual = botoesAbas[i];
+  const funcaoDeClique = criarFuncaoDeClique(i);
+  botaoAtual.addEventListener('click', funcaoDeClique);
 }
-
-botoesAbas.forEach(botao => {
-    botao.addEventListener('click', aoClicarNaAba);
-});
