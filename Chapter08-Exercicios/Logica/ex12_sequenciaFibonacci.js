66eaac01bd95d15ebd8d79e56cd1ce29d196c5bd
Marcio Junior <marciojsjunior.mj@gmail.com>
2025-09-18 18:50:28 -0300

    p

--- Chapter08-Exercicios/Logica/ex12_sequenciaFibonacci.js
+++ Chapter08-Exercicios/Logica/ex12_sequenciaFibonacci.js
@@ -1,24 +1,21 @@
-function gerarFibonacci(n) {
-    if (n <= 0) {
-        return [];
-    }
+function funcaoFibonacci(n) {
+  if (n <= 0) {
+    return [];
+  }
 
-    const sequencia = new Array(n);
-    sequencia[0] = 0;
+  const sequencia = new Array(n);
+  sequencia[0] = 0;
 
-    if (n > 1) {
-        sequencia[1] = 1;
-    }
+  if (n >= 1) {
+    sequencia[1] = 1;
+  }
 
-    for (let i = 2; i < n; i++) {
-        sequencia[i] = sequencia[i - 1] + sequencia[i - 2];
-    }
+  for (let i = 2; i < n; i++) {
+    sequencia[i] = sequencia[i - 1] + sequencia[i - 2];
+  }
 
-    return sequencia;
+  return sequencia;
 }
 
-
-console.log("--- Gerando Fibonacci sem '.push()' ---");
-console.log("Os 10 primeiros números são:", gerarFibonacci(10));
-console.log("Os 2 primeiros números são:", gerarFibonacci(2));
-console.log("O primeiro número é:", gerarFibonacci(1));
+const n = 100;
+console.log("Sequência de Fibonacci:", funcaoFibonacci(n));
---
+++ Chapter08-Exercicios/Logica/ex13_removerDuplicados.js
@@ -0,0 +1,7 @@
+function removerDuplicatas(lista) {
+  const sequencia = new Array();
+
+  for (let i = 0; i < lista; i++) {
+    if (sequencia[i]=)
+  }
+}
