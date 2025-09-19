a014cecac738cb30cc2b79d7bab404b69c820724
Marcio Junior <marciojsjunior.mj@gmail.com>
2025-09-19 15:35:19 -0300

    add ex14-logica

--- Chapter08-Exercicios/Logica/ex13_removerDuplicados.js
+++ Chapter08-Exercicios/Logica/ex13_removerDuplicados.js
@@ -1,7 +1,15 @@
-function removerDuplicatas(lista) {
-  const sequencia = new Array();
+const sequenciaFinal = [];
 
-  for (let i = 0; i < lista; i++) {
-    if (sequencia[i]=)
+function removerDuplicatas(lista) {
+  for (let i = 0; i < lista.length; i++) {
+    const elemento = lista[i];
+    if (!sequenciaFinal.includes(elemento)) {
+      sequenciaFinal.push(elemento);
+    }
   }
+  return sequenciaFinal;
 }
+
+const listaSuja = [1, 2, 3, 3, 4, 4, 4, 5, 6, 7, 7, 7, 8, 9];
+console.log("A versao original da lista com duplicados é:", listaSuja);
+console.log("A lista sem duplicatas é: ", removerDuplicatas(listaSuja));
