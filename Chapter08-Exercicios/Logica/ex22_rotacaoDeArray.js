853ffd83308215e3433f2106b809980cf14d2b0e
Marcio <marciojsjunior.mj@gmail.com>
2025-09-26 07:08:57 -0300

    y

--- Chapter08-Exercicios/Logica/ex22_rotacaoDeArray.js
+++ Chapter08-Exercicios/Logica/ex22_rotacaoDeArray.js
@@ -1,16 +1,24 @@
-const rotacaoArr = (arr, n) => {
-  let arrNovo = [];
-  let numeroDivisor = 0;
+const rotacionarArray = (arr, rotacoes) => {
+  let rotacoesReais = rotacoes % arr.length;
+  let arrNovo = [...arr];
+  let ultimoNumero = 0;
   const tamanhoN = arr.length;
-  let quantasRotacoes = n;
 
-  if (n>tamanhoN){
-    numeroDivisor = quantasRotacoes % tamanhoN;
-  } else {
-    numeroDivisor = n;
+  for (let r = 0; r < rotacoesReais; r++) {
+    ultimoNumero = arrNovo[tamanhoN - 1];
+    for (let i = tamanhoN - 1; i > 0; i--) {
+      arrNovo[i] = arrNovo[i - 1];
+    }
+    arrNovo[0] = ultimoNumero;
   }
-  for (let i = 0; i < numeroDivisor; i++) {
-    elemento = arr[i];
-    arr[i] = 
-  }
+  return arrNovo;
 };
+
+const primeiroArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const rotacoesPropostas = 8;
+console.log(
+  `O array ${primeiroArray} rotacionado ${rotacoesPropostas} vezes, fica assim: ${rotacionarArray(
+    primeiroArray,
+    rotacoesPropostas
+  )}`
+);
