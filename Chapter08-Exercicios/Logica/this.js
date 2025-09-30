c7a5635acebfa6f0ebd6b23758bfb3ed2260eb5d
Marcio Junior <marciojsjunior.mj@gmail.com>
2025-09-29 22:14:51 -0300

    h

--- Chapter08-Exercicios/Logica/this.js
+++ Chapter08-Exercicios/Logica/this.js
@@ -1,12 +1,14 @@
-const carrinho = { // Ao declararmos que isso será um objeto
-  items: [],
-  subtotal: 0.0,
+class Carrinho {
+  constructor() {
+    this.items = [];
+    this.subtotal = 0.0;
+  }
 
   adicionarItem(item) {
     this.items.push(item);
     console.log(`"${item.nome}" foi adicionado ao carrinho.`);
     this.recalcularSubtotal();
-  },
+  }
 
   removerItem(indice) {
     if (indice > -1 && indice < this.items.length) {
@@ -14,14 +16,14 @@ const carrinho = { // Ao declararmos que isso será um objeto
       console.log(`"${itemRemovido[0].nome}" foi removido.`);
       this.recalcularSubtotal();
     }
-  },
+  }
 
   recalcularSubtotal() {
     this.subtotal = 0;
     for (let i = 0; i < this.items.length; i++) {
       this.subtotal += this.items[i].preco;
     }
-  },
+  }
 
   mostrarResumo() {
     console.log("\n--- Resumo do Carrinho ---");
@@ -31,27 +33,22 @@ const carrinho = { // Ao declararmos que isso será um objeto
     });
     console.log(`Subtotal: R$ ${this.subtotal.toFixed(2)}`);
     console.log("------------------------\n");
-  },
-};
-
-// No mundo real, importaríamos o módulo
-// const carrinho = require('./carrinho.js');
+  }
+}
 
 const produto1 = { nome: "Headphone Bluetooth", preco: 249.9 };
 const produto2 = { nome: "Teclado Mecânico", preco: 350.0 };
 const produto3 = { nome: "Mouse Gamer", preco: 125.5 };
 
-// 1. O utilizador adiciona produtos ao carrinho
-carrinho.adicionarItem(produto1);
-carrinho.adicionarItem(produto2);
+const meuCarrinho = new Carrinho();
 
-// 2. Vemos o estado atual do carrinho
-carrinho.mostrarResumo();
+meuCarrinho.adicionarItem(produto1);
+meuCarrinho.adicionarItem(produto2);
 
-// 3. O utilizador adiciona mais um item
-carrinho.adicionarItem(produto3);
-carrinho.mostrarResumo();
+meuCarrinho.mostrarResumo();
 
-// 4. O utilizador decide remover o primeiro item (Headphone)
-carrinho.removerItem(0);
-carrinho.mostrarResumo();
+meuCarrinho.adicionarItem(produto3);
+meuCarrinho.mostrarResumo();
+
+meuCarrinho.removerItem(0);
+meuCarrinho.mostrarResumo();
