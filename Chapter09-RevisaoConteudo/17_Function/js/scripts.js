68e2fd915e9fbc47ae5f9920df12ee0a4f80382a
Marcio <marciojsjunior.mj@gmail.com>
2025-10-01 18:31:05 -0300

    o

--- Chapter09-RevisaoConteudo/17_Function/js/scripts.js
+++ Chapter09-RevisaoConteudo/17_Function/js/scripts.js
@@ -1,10 +1,30 @@
+console.log("Imprimir nomes:");
+
 function primeiraFuncao() {
-  console.log('Hello world da funções');
+  console.log("Hello world da funções");
 }
 primeiraFuncao();
 
-function dizerNome(Aluno){
+function dizerNome(Aluno) {
   console.log(`O nome dele é: ${Aluno}`);
 }
-const nomeDoAluno = "João"
+const nomeDoAluno = "João";
+const nomeDoAluno2 = "Marcio";
+const nomeDoAluno3 = "Bianca";
 dizerNome(nomeDoAluno);
+dizerNome(nomeDoAluno2);
+dizerNome(nomeDoAluno3);
+
+// --------------------------------------------
+
+console.log("Somas:");
+
+function soma(a, b) {
+  let somaDeVariaveis = a + b;
+  return somaDeVariaveis;
+}
+
+let somaUm = soma(2, 5);
+let somaDois = soma(5623, 4534);
+console.log(somaUm);
+console.log(somaDois);
---
+++ Chapter09-RevisaoConteudo/18_Dom(For)/js/scripts.js
@@ -0,0 +1,22 @@
+const lista = ["Laranja", "Maça", "Pera", "Jaca", "Limão"];
+
+// Aqui criamos uma lista e adicionamos na const listaUl.
+const listaUl = document.createElement("ul");
+
+// Aqui selecionamos todo o <body></body>.
+const body = document.getElementsByTagName("Body");
+
+// console.log(body[0]);
+
+body[0].appendChild(listaUl);
+
+const listaNoBody = document.getElementsByTagName("ul");
+
+console.log(listaNoBody[0]);
+
+for (var i = 0; i < lista.length; i++) {
+  const liFor = document.createElement("li");
+  const textoLi = document.createTextNode(lista[i]);
+  liFor.appendChild(textoLi);
+  listaNoBody[0].appendChild(liFor);
+}                       
