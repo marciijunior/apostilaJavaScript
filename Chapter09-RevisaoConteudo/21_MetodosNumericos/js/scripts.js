7b2c4cea0fd03c1756b2e8f3dfef6e10875261d0
Marcio <marciojsjunior.mj@gmail.com>
2025-10-02 21:28:55 -0300

    flaksdj

---
+++ Chapter09-RevisaoConteudo/21_MetodosNumericos/js/scripts.js
@@ -0,0 +1,26 @@
+// parseFloat 10.0
+
+console.log(parseFloat("12.123"));
+console.log(Number.parseFloat("12.123")); // É a mesma cois que sem, acima ele está subintendido;
+
+// parseInt 10
+
+console.log(parseInt("10"));
+console.log(parseInt(16.96));
+
+// toFixed
+
+console.log((23.435235542).toFixed(1));
+
+// isNaN
+
+console.log(isNaN("teste"));
+console.log(isNaN(12));
+console.log(isNaN("14"));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.6562234523452345654634e342);
